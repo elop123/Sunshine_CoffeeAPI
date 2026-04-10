c5a3ee9f30c6efbdc1d395e04fa167e33853ab9f
elop123 <142408289+elop123@users.noreply.github.com>
2026-04-10 15:22:17 +0200

    Add PORT configuration for database connection

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,7 @@
 // Config file - change this to fit your DB
 module.exports = {
   HOST: process.env.DB_HOST,
+  PORT: process.env.DB_PORT || 4000,
   USER: process.env.DB_USER,
   PASSWORD: process.env.DB_PASSWORD,
   DB: process.env.DB_NAME,
