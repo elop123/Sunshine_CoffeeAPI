6b2249c309bf451ac0d520a9423be312e36dbfb8
elop123 <142408289+elop123@users.noreply.github.com>
2026-04-10 14:41:05 +0200

    Update database configuration environment variables

--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,10 @@
 // Config file - change this to fit your DB
 module.exports = {
-  HOST: process.env.HOST,
-  USER: process.env.USER,
-  PASSWORD: process.env.PASSWORD,
-  DB: process.env.DB,
-  dialect: process.env.DIALECT,
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
+  dialect: "mysql",
   pool: {
     max: 5,
     min: 0,
