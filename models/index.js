a69530a0b09acd8af3c5589a043511c553987397
elop123 <142408289+elop123@users.noreply.github.com>
2026-04-10 14:42:09 +0200

    Refactor Sequelize initialization in index.js

--- models/index.js
+++ models/index.js
@@ -1,33 +1,30 @@
 const dbConfig = require("../config/db.config.js");
-
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: false,
-
-  pool: {
-    max: dbConfig.max,
-    min: dbConfig.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle,
-  },
-  dialectOptions: {
-  ssl: {
-    require: true,
-    rejectUnauthorized: true
+const sequelize = new Sequelize(
+  dbConfig.DB,
+  dbConfig.USER,
+  dbConfig.PASSWORD,
+  {
+    host: dbConfig.HOST,
+    dialect: "mysql",
+    logging: false,
+    pool: dbConfig.pool,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true,
+      },
+    },
   }
-},
-  dialect: "mysql"
-});
+);
 
 const db = {};
-
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 db.Product = require("../models/product.model.js")(sequelize, Sequelize);
 db.User = require("../models/user.model.js")(sequelize, Sequelize);
 db.AuthToken = require("../models/authToken.model.js")(sequelize, Sequelize);
+
 module.exports = db;
